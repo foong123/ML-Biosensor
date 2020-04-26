213f01a1ea2e57bc0622d16e3d5c5ed84b086fae
pi@raspberrypi <vauxyf@gmail.com>
2020-04-26 22:27:45 +0800

    changed some scripts to suit for raspberrypi usage

--- GUI/demo.py
+++ GUI/demo.py
@@ -6,19 +6,8 @@ import torch
 import torch.nn.functional as F
 import sys
 
-print ('Argument List:', str(sys.argv))
-
-import threading
-import time
-
-class ThreadedTask(threading.Thread):
-    def __init__(self, progress):
-        threading.Thread.__init__(self)
-        self.progress = progress
-    def run(self):
-        for i in range(100):
-            self.progress.step(1)  # Update progress bar
-            time.sleep(1)  # Simulate long running process
+#print ('Argument List:', str(sys.argv))
+#print (str(sys.argv[1]))
 
 # Import tensor dataset & data loader
 from torch.utils.data import TensorDataset, DataLoader
@@ -26,8 +15,8 @@ from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
 from sklearn.preprocessing import StandardScaler
 from torch.autograd import Variable
 
-train = pd.read_csv("D:/unmc degree/year 4/fyp/project_git/DPV/train_dpv.csv")
-test = pd.read_csv("D:/unmc degree/year 4/fyp/project_git/DPV/test_dpv.csv")
+train = pd.read_csv("/home/pi/Desktop/fyp/project/dataset/DPV/train_dpv.csv")
+test = pd.read_csv("{}".format(str(sys.argv[1])))
 
 X_train = train.drop('ID', axis=1).drop('label', axis=1).values
 y_train = train['label'].values
@@ -69,42 +58,47 @@ class Regression(nn.Module):
         return x
 
 # Model class must be defined somewhere
-model = torch.load("D:/unmc degree/year 4/fyp/project_git/regression_model.pth")
+model = torch.load("/home/pi/Desktop/fyp/project/panadol_prediction/regression_model.pth")
 
 #Predicting for X_test
 y_pred = model(X_test)
 y_pred = y_pred.detach().numpy()
 
-y_pred = [y_pred[x].item() for x in range(len(y_pred))]
-y_test = [y_test[x].item() for x in range(len(y_test))]
+f = open("/home/pi/Desktop/fyp/project/GUI/Panadol_results/Panadol_result.txt", "w+")
+f.write("Concentration of Panadol detected is: {}".format(y_pred))
+f.write("\n")
+f.close()
+
+#y_pred = [y_pred[x].item() for x in range(len(y_pred))]
+#y_test = [y_test[x].item() for x in range(len(y_test))]
 
 # Comparing Actual and predicted values
-df = {}
-df['Actual Concentration'] = y_test
-df['Predicted Concentration'] = y_pred
-df = pd.DataFrame(df)
-print(df)
+#df = {}
+#df['Actual Concentration'] = y_test
+#df['Predicted Concentration'] = y_pred
+#df = pd.DataFrame(df)
+#print(df)
 
-percent_error = 0.0
+#percent_error = 0.0
 
-for i in range(len(y_pred)):
+#for i in range(len(y_pred)):
 
-    a = float(y_pred[i])
-    b = float(y_test[i])
-    c = abs(a-b)/b *100.0
-    percent_error = c + percent_error
+#    a = float(y_pred[i])
+#    b = float(y_test[i])
+#    c = abs(a-b)/b *100.0
+#    percent_error = c + percent_error
       
-percent_error = percent_error/len(y_pred)
+#percent_error = percent_error/len(y_pred)
 
-print('percent_error: ', percent_error)
-print('R2: ', r2_score(y_pred = y_pred, y_true = y_test))
-print('mean squared error: ', mean_squared_error(y_pred = y_pred, y_true = y_test))
-print('mean absolute error: ', mean_absolute_error(y_pred = y_pred, y_true = y_test))
+#print('percent_error: ', percent_error)
+#print('R2: ', r2_score(y_pred = y_pred, y_true = y_test))
+#print('mean squared error: ', mean_squared_error(y_pred = y_pred, y_true = y_test))
+#print('mean absolute error: ', mean_absolute_error(y_pred = y_pred, y_true = y_test))
 
 #Visualizing Actual and predicted values
-plt.plot(y_test, color = 'red', label = 'Real data')
-plt.plot(y_pred, color = 'blue', label = 'Predicted data')
-plt.title(percent_error)
-plt.legend()
-plt.grid()
-plt.show()
+#plt.plot(y_test, color = 'red', label = 'Real data')
+#plt.plot(y_pred, color = 'blue', label = 'Predicted data')
+#plt.title(percent_error)
+#plt.legend()
+#plt.grid()
+#plt.show()
