531d95eb4ae88925214ccdddc8c5533bb3745a86
Foong Pak Chuen <kecy6fpc@nottingham.edu.my>
2020-05-23 22:31:01 +0800

    added icon

--- GUI/main.py
+++ GUI/main.py
@@ -1,9 +1,6 @@
-from tkinter import filedialog
-from tkinter import *
-from tkinter import ttk
 import time
 import sys
-import tkinter as tk
+import os
 
 sys.path.insert(1, '/home/pi/Desktop/fyp/project/panadol_prediction/')
 sys.path.insert(1, '/home/pi/Desktop/fyp/project/dengue_detection/')
@@ -11,11 +8,15 @@ from demo_ann_Panadol import Thread1, Regression
 from demo_ann_Dengue import Thread2, NN_Model
 from demo_svm_Panadol import Thread3
 from demo_svm_Dengue import Thread4
+from PIL import ImageTk, Image
+from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk
 import os
 
 currentDirectory = os.getcwd()
 window = Tk()
-#window.iconphoto(False, tk.PhotoImage(file = '/home/pi/Desktop/fyp/project/GUI/GUI_images/biosensor_icon.png'))
+#window.iconphoto(False, PhotoImage(file = '/home/pi/Desktop/fyp/project/GUI/GUI_images/biosensor_icon.png'))
 
 #Utilities functions
 def all_children(window):
@@ -35,14 +36,33 @@ def Home_page():
     for item in widget_list:
         item.destroy()
 
-    window.title("FYP")
-    #Window size
-    window.geometry('500x200')      #Window size
+    #Center window
+    window.resizable(False, False)  # This code helps to disable windows from resizing
 
-    #Title 
-    Title = Label(window, text="Welcome!", font=("Arial Bold", 30))
-    Title.config(anchor = CENTER)
-    Title.pack()
+    #window size
+    window_height = 300
+    window_width = 700
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_cordinate = int((screen_width/2) - (window_width/2))
+    y_cordinate = int((screen_height/2) - (window_height/2))
+
+    window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+    
+    #window title
+    window.title("FYP")
+    
+    #Logo
+    img = Image.open("D:/unmc degree/year 4/fyp/project_git/welcome_icon.png")
+    img = img.resize((170, 160), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = Label(window, image=img)
+    panel.image = img
+    panel.config(anchor = CENTER)
+    panel.pack()
+
+
     #Panadol concentration estimation button
     Panadol_btn = Button(window, text="Panadol concentration estimation", command = Panadol_task)
     Panadol_btn.place(x=10, y=100)
@@ -57,22 +77,32 @@ def Home_page():
 
 #About page
 def About_page():
-
-    #window = Tk()
+    
     window.title("FYP - About")
-    #Window size
-    window.geometry('400x200')      #Window size
+    window.geometry("600x430")
+
     #About_title
     About_title = Label(window, text="About", font=("Arial Bold", 20))
     About_title.config(anchor = CENTER)
     About_title.pack()
     #Description 
-    About_Description = Label(window, text="A simple UI for ML based post analysis",font=("Arial Bold", 12))
+    About_Description = Label(window, 
+                            text="A simple GUI for ML based post analysis. This is part of the final year project.Created by Foong Pak Chuen, MEng. Electrical and Electronic Engineering Student",
+                            font=("Arial Bold", 12),
+                            wraplength=500)
     About_Description.place(x=0, y=50)
+
+    #Logo
+    img = Image.open("D:/unmc degree/year 4/fyp/project_git/UNMC_logo.png")
+    img = img.resize((450, 250), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = Label(window, image=img)
+    panel.image = img
+    panel.place(x=50, y=150)
 
     #Back button
     Back_btn = Button(window, text="Back", command = Return_Home)
-    Back_btn.place(x=150, y=150)
+    Back_btn.place(x=0, y=10)
 
 #Task Descripton page
 def Task_description_page(): 
@@ -90,8 +120,8 @@ def Task_description_page():
 
     #window = Tk()
     window.title("FYP - {}".format(title))
-    #Window size
-    window.geometry('500x150')      #Window size
+    window.geometry("500x130")
+
     #Description 
     Description_1 = Label(window, text=text,font=("Arial Bold", 12))
     Description_1.place(x=0, y=30)
@@ -129,15 +159,13 @@ def Tutorial_Page():
         file = "EIS"
         file_dimension = "6"
 
-    #Window size
-    window.geometry('700x250')      #Window size
-
     #Title
     title = Label(window, text="{} Detection - Tutorial".format(title), font=("Arial Bold", 20))
     title.config(anchor = CENTER)
     title.pack()
 
-    window.title("FYP - {} Detection - Tutorial".format(title))    
+    window.title("FYP - {} Detection - Tutorial".format(title))
+    window.geometry("700x250")    
 
     Description_1 = Label(window, text="1) Click Start and insert the {} sensor file.".format(file), font=("Arial Bold", 10))
     Description_1.place(x=0, y=40)
@@ -175,7 +203,10 @@ def Browse_File_Page():
     elif task == 2:
         title = "Dengue"
         file = "EIS"
-    
+
+    window.title("FYP - {} Detection - Select Test File".format(title))
+    window.geometry("600x300")
+
     #Browse Button
     Browse_button = Button(window, text = "Browse A File", command = fileDialog)
     Browse_button.place(x=0, y=100)
@@ -184,9 +215,6 @@ def Browse_File_Page():
     Back_button = Button(window, text="Back", command = Task_Back_button_clicked)
     Back_button.place(x=300, y=200)
 
-    window.title("FYP - {} Detection - Select Test File".format(title))
-    #Window size
-    window.geometry('600x300')      #Window size
     #Description
     Description_1 = Label(window, text="Select a {} sensor file to estimate the Panadol Concentration".format(file), font=("Arial Bold", 10))
     Description_1.place(x=0, y=60)
@@ -212,8 +240,7 @@ def Result_Page():
         result = open("/home/pi/Desktop/fyp/project/GUI/ML_results/Dengue_results/Dengue_result.txt", "r")
 
     window.title("FYP - {} Result".format(title))
-    #Window size
-    window.geometry('400x400')      #Window size
+    window.geometry("400x400")
 
     #Print Results from text file
     a = result.read()
